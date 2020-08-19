78aabfda7f7d670660fc350dc306b5e3abbff54d
Gregor Martynus <gregor@martynus.net>
2020-08-19 13:34:34 -0700

    ci: create pull request when nodejs.org/dist/index.json changes

--- scripts/update.js
+++ scripts/update.js
@@ -1,1 +1,15 @@
-console.log("Hello from the update workflow");
+const { writeFileSync } = require("fs");
+
+const bent = require("bent");
+const getJSON = bent("json");
+
+run();
+
+async function run() {
+  const nodeVersions = await getJSON("https://nodejs.org/dist/index.json"); // read the remote JSON from an official source
+  writeFileSync(
+    "cache/index.json",
+    JSON.stringify(nodeVersions, null, 2) + "\n"
+  );
+  console.log("cache/index.json written");
+}
