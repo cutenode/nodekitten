7c16b5a3c3b8bd3606c1e4ffaad4cded56bd23bc
Gregor Martynus <gregor@martynus.net>
2020-08-27 11:49:09 -0700

    ci(notify): debug.3

--- scripts/notify.js
+++ scripts/notify.js
@@ -47,88 +47,86 @@ async function main() {
     console.log(`newVersions`);
     console.log(newVersions);
 
-    return;
+    // if (!diffString) {
+    //   console.log("No change found. Bye.");
+    //   return;
+    // }
 
-    if (!diffString) {
-      console.log("No change found. Bye.");
-      return;
-    }
+    // console.log("Diff found");
+    // console.log(diffString);
 
-    console.log("Diff found");
-    console.log(diffString);
+    // const newLines = diffString
+    //   .split("\n")
+    //   .filter((line) => /^\+  /.test(line))
+    //   .map((line) => line.substr(3));
+    // const jsonString = `[ ${newLines.join("\n").replace(/,$/, "")} ]`;
 
-    const newLines = diffString
-      .split("\n")
-      .filter((line) => /^\+  /.test(line))
-      .map((line) => line.substr(3));
-    const jsonString = `[ ${newLines.join("\n").replace(/,$/, "")} ]`;
+    // const newVersions = JSON.parse(jsonString);
 
-    const newVersions = JSON.parse(jsonString);
+    // const octokit = new OctokitWithPagination({
+    //   auth: {
+    //     id: APP_ID,
+    //     privateKey: PRIVATE_KEY,
+    //   },
+    //   authStrategy: createAppAuth,
+    // });
 
-    const octokit = new OctokitWithPagination({
-      auth: {
-        id: APP_ID,
-        privateKey: PRIVATE_KEY,
-      },
-      authStrategy: createAppAuth,
-    });
+    // const installations = await octokit.paginate("GET /app/installations", {
+    //   mediaType: { previews: ["machine-man"] },
+    //   per_page: 100,
+    // });
 
-    const installations = await octokit.paginate("GET /app/installations", {
-      mediaType: { previews: ["machine-man"] },
-      per_page: 100,
-    });
+    // for (const {
+    //   id,
+    //   account: { login },
+    // } of installations) {
+    //   console.log("Installation found: %s (%d)", login, id);
 
-    for (const {
-      id,
-      account: { login },
-    } of installations) {
-      console.log("Installation found: %s (%d)", login, id);
+    //   const installationOctokit = new OctokitWithPagination({
+    //     auth: {
+    //       id: APP_ID,
+    //       privateKey: PRIVATE_KEY,
+    //       installationId: id,
+    //     },
+    //     authStrategy: createAppAuth,
+    //   });
 
-      const installationOctokit = new OctokitWithPagination({
-        auth: {
-          id: APP_ID,
-          privateKey: PRIVATE_KEY,
-          installationId: id,
-        },
-        authStrategy: createAppAuth,
-      });
+    //   const repositories = await installationOctokit.paginate(
+    //     "GET /installation/repositories",
+    //     {
+    //       mediaType: { previews: ["machine-man"] },
+    //       per_page: 100,
+    //     }
+    //   );
 
-      const repositories = await installationOctokit.paginate(
-        "GET /installation/repositories",
-        {
-          mediaType: { previews: ["machine-man"] },
-          per_page: 100,
-        }
-      );
+    //   console.log(
+    //     "Repositories found on %s: %d. Dispatching events",
+    //     login,
+    //     repositories.length
+    //   );
 
-      console.log(
-        "Repositories found on %s: %d. Dispatching events",
-        login,
-        repositories.length
-      );
+    //   for (const { name, full_name } of repositories) {
+    //     for (const newVersion of newVersions) {
+    //       const options = installationOctokit.request.endpoint.merge(
+    //         "POST /repos/:owner/:repo/dispatches",
+    //         {
+    //           owner: login,
+    //           repo: name,
+    //           event_type: "nodekitten",
+    //           client_payload: newVersion,
+    //         }
+    //       );
+    //       console.log(options);
 
-      for (const { name, full_name } of repositories) {
-        for (const newVersion of newVersions) {
-          const options = installationOctokit.request.endpoint.merge(
-            "POST /repos/:owner/:repo/dispatches",
-            {
-              owner: login,
-              repo: name,
-              event_type: "nodekitten",
-              client_payload: newVersion,
-            }
-          );
-          console.log(options);
-
-          await installationOctokit.request(options);
-          console.log(
-            "Event distpatched for %s and %s",
-            full_name,
-            newVersion.version
-          );
-        }
-      }
-    }
+    //       await installationOctokit.request(options);
+    //       console.log(
+    //         "Event distpatched for %s and %s",
+    //         full_name,
+    //         newVersion.version
+    //       );
+    //     }
+    //   }
+    // }
   } catch (error) {
     console.log(error);
     throw error;
